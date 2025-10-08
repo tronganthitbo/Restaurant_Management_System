917e504926d7296cbcb21d5fb08e66286b3a4833
Truong Binh Trong <trongtb.ce190757@gmail.com>
2025-10-08 17:14:05 +0700

    Update DBContext file

---
+++ src/main/java/utils/DBContext.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author
+ */
+public class DBContext {
+    
+        protected Connection conn = null;
+
+    public DBContext() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String dbURL = "jdbc:sqlserver://localhost:1433;"
+                    + "databaseName=RestaurantManagement;"
+                    + "user=sa;"
+                    + "password=6789;"
+                    + "encrypt=true;trustServerCertificate=true;";
+            conn = DriverManager.getConnection(dbURL);
+            if (conn != null) {
+                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: "
+                        + dm.getDatabaseProductName());
+                System.out.println("Product version: "
+                        + dm.getDatabaseProductVersion());
+            }
+        } catch (SQLException ex) {
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }  
+}
+
