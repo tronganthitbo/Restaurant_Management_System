6c025907c5f31394d97c7cd8ef6cbc29b1b67e7f
Dai Minh Nhu <NhuDM.CE190213@gmail.com>
2025-10-10 15:29:24 +0700

    Up lai role model

---
+++ src/main/java/model/Role.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Dai Minh Nhu - CE190213
+ */
+public class Role {
+
+    private int role_id;
+    private String role_name;
+    private String description;
+    private String status;
+
+    public Role(int role_id, String role_name, String description, String status) {
+        this.role_id = role_id;
+        this.role_name = role_name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+
+    public String getRole_name() {
+        return role_name;
+    }
+
+    public void setRole_name(String role_name) {
+        this.role_name = role_name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
